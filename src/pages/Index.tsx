cd457143d88f0e59c90c951b156a1135a2058f84
Ramharsh-aidev <ramharshdandekar@gmail.com>
2025-08-22 13:37:41 +0530

    Added basic structure for the home page

---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+import HeroSection from '../components/HeroSection';
+import FeaturesSection from '../components/FeaturesSection';
+import SubjectsSection from '../components/SubjectsSection';
+
+const Index = () => {
+  return (
+    <main className="relative">
+      <HeroSection />
+      <FeaturesSection />
+      <SubjectsSection />
+    </main>
+  );
+};
+
+export default Index;
